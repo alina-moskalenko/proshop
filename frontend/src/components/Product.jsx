009bdd0bf222fbcde554eb0c8fe4384047b2a04b
alina-moskalenko <alinaamoskalenko@gmail.com>
2024-03-20 20:00:50 -0700

    implemented react router to header and homescreen

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,16 +1,17 @@
 import { Card, CardBody } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} varient="top"></Card.Img>
-      </a>
+      </Link>
       <CardBody>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div"></Card.Title>
           <strong>{product.name}</strong>
-        </a>
+        </Link>
 
         <Card.Text as="h3">${product.price}</Card.Text>
       </CardBody>
